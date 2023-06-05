846bcce637d1a4d3b07071f0d73b8d6dc74cd8e0
suganyapachiappan <135566249+suganyapachiappan@users.noreply.github.com>
2023-06-05 16:49:07 +0530

    Update Anagram.java

--- src/Anagram.java
+++ src/Anagram.java
@@ -4,7 +4,7 @@ public class Anagram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+int a=0;
 		String s1 = "Dusty";
 		String s2 = "Study";
 		 s1 = s1.toLowerCase();
